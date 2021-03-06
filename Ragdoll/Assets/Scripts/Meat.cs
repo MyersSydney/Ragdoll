67b889a845327211992b3397001e417a40adf82d
ZAG852 <36207290+ZAG852@users.noreply.github.com>
2021-03-05 23:36:31 -0600

    Added Game manager

--- Ragdoll/Assets/Scripts/Meat.cs
+++ Ragdoll/Assets/Scripts/Meat.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[CreateAssetMenu(fileName = "Meat", menuName = "ScriptedObject/Meat", order = 3)]
 public class Meat : Item
 {
     public enum howDone {rare, mediumRare, Done, WellDone}
