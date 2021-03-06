28a30ae06ffba7454525c498460d1b6744eaa51d
ZAG852 <36207290+ZAG852@users.noreply.github.com>
2021-03-05 23:24:39 -0600

    The recipe and item and meat scriptable templates are created.

---
+++ Ragdoll/Assets/Scripts/Item.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "ScriptedObject/Item", order = 1)]
+public class Item : ScriptableObject
+{
+    public enum Quality {poor, good, great, amazing, awesome, theBomb}
+    public string myName;
+    public string description;
+    public int points;
+    public Quality quality;
+    public int price;
+}
---
+++ Ragdoll/Assets/Scripts/Recipe.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Recipe", menuName = "ScriptedObject/Recipe", order = 2)]
+public class Recipe : ScriptableObject
+{
+    public Item[] items;
+    public Item createdItem;
+}
