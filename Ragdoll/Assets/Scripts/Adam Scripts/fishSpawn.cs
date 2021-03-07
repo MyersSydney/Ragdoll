8106c7ec78f753b47b7df24f64e59e6d102cca92
Zaidis <chewybinx@gmail.com>
2021-03-07 15:13:31 -0600

    dsa

--- Ragdoll/Assets/Scripts/Adam Scripts/fishSpawn.cs
+++ Ragdoll/Assets/Scripts/Adam Scripts/fishSpawn.cs
@@ -6,19 +6,20 @@ public class fishSpawn : MonoBehaviour
 {
 
     public GameObject fish;
-    private Vector3 Min;
-    private Vector3 Max;
-    float x;
-    float y;
-    float z;
-
-    Vector3 RandomPosition;
-    public bool canSpawn;
-    private void Start() {
-        
+    public float timer;
+    private void Update() {
+        timer -= Time.deltaTime;
+        if(timer <= 0) {
+            SpawnFish();
+            timer = 3;
+        }
     }
-
-    void SetRanges() {
-        
+    void SpawnFish() {
+        int rand1 = Random.Range(1, 30); //z
+        int rand2 = Random.Range(3, 30); //y
+        //x = 80
+        GameObject obj = Instantiate(fish, new Vector3(80, rand2, rand1), Quaternion.identity);
+        obj.GetComponent<Renderer>().material.color = new Color32((byte)Random.Range(0, 255), (byte)Random.Range(0, 255), (byte)Random.Range(0, 255), 255);
+        obj.transform.rotation = Quaternion.Euler(0, 0, 0);
     }
 }
