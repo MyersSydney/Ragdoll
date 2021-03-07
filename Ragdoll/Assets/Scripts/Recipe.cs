6283f8c3d01fb3d2c312c22335632e89b2487b60
ZAG852 <36207290+ZAG852@users.noreply.github.com>
2021-03-07 16:21:03 -0600

    its fixed

--- Ragdoll/Assets/Scripts/Recipe.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(fileName = "Recipe", menuName = "ScriptedObject/Recipe", order = 2)]
-public class Recipe : ScriptableObject
-{
-    public List<Item> items = new List<Item>();
-
-}
